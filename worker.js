4ff8baaaa160191750359e02c652e5d24b33de1f
Nathan Clevenger <nateclev@gmail.com>
2022-09-27 14:54:03 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -24,9 +24,9 @@ export default {
     const args = argArray.map(arg => ({ name: arg.split('=')[0], default: arg.split('=')[0] }))
     const isCode = decodeURI(pathSegments[2]).includes('=>')
     const code = decodeURI(pathSegments[2])
-    const importUrl = `import func from '${url}'`
+    const importUrl = `// Generated at ${url}\n${isCode ? `import func from '${url}'` : ''
     const template = importUrl + `
-import ctx from 'https://pkg.do/ctx.do@1.0.6'
+// import ctx from 'https://pkg.do/ctx.do@1.0.6'
 
 export const api = {
   icon: '👌',
@@ -46,8 +46,9 @@ export const api = {
 
 export default {
   fetch: async (req, ctx) => {
-    const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query, search, hash } = await ctx.fetch(req).then(res => res.json())
-    const [${args.map(arg => arg.name + ' = ' + arg.default).join(', ')}] = pathSegments
+    // const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query, search, hash } = await ctx.fetch(req).then(res => res.json())
+    const { hostname, pathname, searchParams } = new URL(req.url)
+    const [${args.map(arg => arg.name + ' = ' + arg.default).join(', ')}] = pathname.split('/')
     const func = (${args}) => ${code}
     const results = await func(${args.map(arg => arg.name).join(', ')},query)
     return new Response(JSON.stringify({api,args,query,results,user}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
