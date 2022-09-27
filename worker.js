afc4d64e5062879a4104d99566f14a68da6ff173
Nathan Clevenger <nateclev@gmail.com>
2022-09-27 10:41:39 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,6 +19,40 @@ export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query, search, hash } = await env.CTX.fetch(req).then(res => res.json())
     if (pathname == '/api') return new Response(JSON.stringify({api,user}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
-    return fetch('https://esb.denoflare.dev/https://' + pathname + search)
+    const isCode = pathSegments[0].includes('=>')
+    const code = pathSegments[0]
+    const importUrl = `import func from '${url}'\n\n`
+    const template = importUrl + `
+import ctx from 'https://pkg.do/ctx.do'
+
+export const api = {
+  icon: '👌',
+  name: 'worker.do',
+  description: 'Generate a Cloudflare Worker from a Function',
+  url: 'https://worker.do/api',
+  type: 'https://apis.do/code',
+  endpoints: {
+    buildCode: 'https://worker.do/:code',
+    buildFile: 'https://worker.do/:url',
+  },
+  site: 'https://worker.do',
+  login: 'https://worker.do/login',
+  signup: 'https://worker.do/signup',
+  subscribe: 'https://worker.do/subscribe',
+  repo: 'https://github.com/drivly/worker.do',
+}
+
+export default {
+  fetch: async (req, ctx) => {
+    const { hostname, pathname, searchParams } = new URL(req.url)
+    const args = pathname.slice(1).split('/')
+    const query = Object.fromEntries(searchParams)
+    const func = ${code}
+    const results = await func([...args],query)
+    return new Response(JSON.stringify({api,args,query,results,user}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  }
+}
+`
+    return new Response(template, { headers: { 'content-type': 'application/javascript; charset=utf-8' }})
   },
 }
